b5775c10258464ce1fb0483f27a9a5c7a68f8161
FerisJumbo <colegartner1@student.isd423.org>
2020-07-24 10:30:40 -0500

    Did some refactoring

--- RPGTilegame/src/dev/FerisJumbo/RPGTilegame/Launcher.java
+++ RPGTilegame/src/dev/FerisJumbo/RPGTilegame/Launcher.java
@@ -7,7 +7,7 @@ public class Launcher {
 	
 	private static String GAMENAME = "RPG Game";
 	private static  char VERSIONMOD = 'a'; // Alpha
-	private static String VERSION = "0.1.5";
+	private static String VERSION = "0.1.6";
 
 	public static void main(String[] args) {
 		// Creates a new game instance
